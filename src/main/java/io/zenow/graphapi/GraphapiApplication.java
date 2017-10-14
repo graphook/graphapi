59d219e97959450e3b7959344954964552f71a76
Jackson Morgan <jaxoncreed@gmail.com>
2017-10-14 13:32:34 -0400

    init

---
+++ src/main/java/io/zenow/graphapi/GraphapiApplication.java
@@ -0,0 +1,12 @@
+package io.zenow.graphapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphapiApplication.class, args);
+	}
+}
