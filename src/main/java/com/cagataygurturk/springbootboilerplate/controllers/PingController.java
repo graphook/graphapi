1f9222684a7d23df55222e5274348c7985743d26
Jackson Morgan <jaxoncreed@gmail.com>
2018-06-12 20:30:20 -0400

    did something

--- src/main/java/com/cagataygurturk/springbootboilerplate/controllers/PingController.java
+++ src/main/java/com/cagataygurturk/springbootboilerplate/controllers/PingController.java
@@ -1,9 +1,8 @@
 package com.cagataygurturk.springbootboilerplate.controllers;
 
-
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.G  ET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
